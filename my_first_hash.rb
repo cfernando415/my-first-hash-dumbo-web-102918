b0d9f447a9251d66ea9770bbf9bd1318dabee8b7
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-11 23:59:08 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   my_info = {"name" => "Carlo Fernando", "age" => 38, "height" = "64 in."}
-
-  
 end
 
 
@@ -10,8 +8,8 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  inventory = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  
 end
 
 def retrieval
